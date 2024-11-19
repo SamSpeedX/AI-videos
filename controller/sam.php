ae2784052a3c3e9e0965f6a69e794f4d357ea61f
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-19 15:50:59 +0300

    Update sam.php

--- controller/sam.php
+++ controller/sam.php
@@ -1,10 +1,10 @@
 <?php
 //header("Content-Type: application/json");
 
-use Simon\Controller\VideoController;
+use Simon\controller\VideoController;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 $videoController = new VideoController();
 echo $videoController->createVideo();
-}
+//}
