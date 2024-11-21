0c496be316c8d0e1a8c0466d86df90114ede21c6
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-21 12:44:24 +0300

    Update sam.php

--- controller/sam.php
+++ controller/sam.php
@@ -1,6 +1,6 @@
 <?php
 //header("Content-Type: application/json");
-
+require_once __DIR__.'../vendor/autoload.php';
 use Simon\controller\VideoController;
 
 //if ($_SERVER['REQUEST_METHOD'] === 'POST') {
